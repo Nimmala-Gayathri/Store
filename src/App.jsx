d4947e2eb75e6549933bea4b0fd93eeebd19ab3d
Nimmala-Gayathri <nimmalagayathri292@gmail.com>
2024-09-30 19:38:17 +0530

    api fetched

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,25 @@ import './App.css'
 import axios from "axios"
 
 function App() {
-  const [products, setProducts] = useState({})
+  const [products, setProducts] = useState([])
   useEffect(()=>{
     const api = axios.get("https://fakestoreapi.com/products").then((res) =>{
       console.log(res.data)
+      setProducts(res.data)
     })
   },[])
   return (
     <div>
       <h1>Store api</h1>
+      {products.map((product,index)=>{
+        return(
+          <div key={index}>
+            <img src={product.image} style={{width:"10rem"}}/>
+            <h6>{product.title}</h6>
+            <p>{product.price}</p>
+          </div>
+        )
+      })}
     </div>
   )
 }
